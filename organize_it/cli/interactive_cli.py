879464502a298e382e15082c9fa4a9d61693dda9
Prasad <prasadkjose@gmail.com>
2024-12-25 22:46:31 +0100

    Input loops and console util

--- organize_it/cli/interactive_cli.py
+++ organize_it/cli/interactive_cli.py
@@ -14,22 +14,8 @@ PRINT = "print"
 ARG = "arg"
 RESPONSE = "response"
 
-
-def help_me():
-    print("you will find help here")
-
-
-def view_tree():
-    print("This is where the tree will be viewed")
-
-
-def set_arg():
-    print("CLI args will be set based on the response")
-
-
-def proceed():
-    print("Proceed next")
-
+# The number of user attempts per input before exiting.
+NUMBER_OF_ATTEMPTS = 5
 
 # TODO: backwards iteration https://stackoverflow.com/questions/55380989/is-there-any-way-to-go-back-a-step-in-a-python-for-loop
 
@@ -50,18 +36,21 @@ class InteractiveCLI:
                 input_dict = prompt_item[INPUT]
                 if PRINT in input_dict:
                     # Prompt the user for input in the 'print'
-                    user_response = input(input_dict[PRINT])
-                    if ARG in input_dict:
-                        # If it's an arg based prompt, then store the response in it
-                        arg_dict[input_dict[ARG]] = user_response
-                    elif RESPONSE in input_dict:
-                        # If it's an response based prompt, then call the appropriate response method from the 'response' key of the input prompt
-                        if user_response in input_dict[RESPONSE]:
-                            response_method = input_dict[RESPONSE][user_response]
-                            globals()[response_method]()  # can be global or local
-                        else:
-                            print("oops wrong input")
-                            exit_gracefully()
+                    for _ in range(NUMBER_OF_ATTEMPTS):
+                        user_response = input(input_dict[PRINT])
+                        if ARG in input_dict:
+                            # If it's an arg based prompt, then store the response in it
+                            arg_dict[input_dict[ARG]] = user_response
+                        elif RESPONSE in input_dict:
+                            # If it's an response based prompt, then call the appropriate response method from the 'response' key of the input prompt
+                            if user_response in input_dict[RESPONSE]:
+                                response_method = input_dict[RESPONSE][user_response]
+                                getattr(
+                                    self, response_method
+                                )()  # can be global or local
+                                break
+                            else:
+                                print("That was an incorrect input. Please try again.")
                     else:
                         # Wrong prompt structure
                         exit_gracefully()
@@ -69,3 +58,24 @@ class InteractiveCLI:
                 # Wrong prompt structure
                 exit_gracefully()
         return arg_dict
+
+    def help_me(self):
+        with open(os.path.join(INPUT_SCRIPT_PATH, "help.txt"), "r") as f:
+            print(f.read())
+        self.__continue_and_clear()
+
+    def view_tree(self):
+        print("This is where the tree will be viewed")
+
+    def set_arg(self):
+        print("CLI args will be set based on the response")
+
+    def proceed(self):
+        print("Proceed next")
+
+    def __continue(self):
+        input("Press enter to continue")
+
+    def __continue_and_clear(self):
+        input("Press enter to continue")
+        os.system("clear")
